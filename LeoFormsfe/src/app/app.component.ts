7ddc59616ae5c0b56a41012bcca2b8b85640c237
Tobert <tobiashoefl@gmail.com>
2021-11-19 13:51:07 +0100

    TH - Added frontend

---
+++ LeoFormsfe/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'LeoFormsfe';
+
+  markdown = `## LeoForms hat __swag__!
+---
+
+### Wahlfächer
+1. Ordered list
+2. Another bullet point
+   - Unordered list
+   - Another unordered bullet
+`;
+
+
+}
