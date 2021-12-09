c414e3a025d54ad9c7c103e540de40662ff93ee2
Meris Besic <besicmeris@gmail.com>
2021-12-09 11:35:06 +0100

    added SurveyRepo

---
+++ backend/src/main/java/at/htl/repositories/SurveyRepository.java
@@ -0,0 +1,7 @@
+package at.htl.repositories;
+
+import at.htl.model.Survey;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
+public class SurveyRepository implements PanacheRepository<Survey> {
+}
