2b6b939ac2d33b388564e321d0e20dd30694d404
Meris Besic <besicmeris@gmail.com>
2021-12-09 11:36:59 +0100

    added AnswerRepo

---
+++ backend/src/main/java/at/htl/repositories/AnswerRepository.java
@@ -0,0 +1,7 @@
+package at.htl.repositories;
+
+import at.htl.model.Answer;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
+public class AnswerRepository implements PanacheRepository<Answer> {
+}
