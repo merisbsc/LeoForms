13b713e47db5bc1ea0b9be090bda988554cb5be4
Meris Besic <besicmeris@gmail.com>
2021-12-09 11:34:42 +0100

    added PollRepo

---
+++ backend/src/main/java/at/htl/repositories/PollRepository.java
@@ -0,0 +1,7 @@
+package at.htl.repositories;
+
+import at.htl.model.Poll;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
+public class PollRepository implements PanacheRepository<Poll> {
+}
