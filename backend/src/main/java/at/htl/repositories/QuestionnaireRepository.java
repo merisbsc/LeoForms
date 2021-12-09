59f2b2d6cf3f5eaf57b064600e0520eacf017799
Meris Besic <besicmeris@gmail.com>
2021-12-09 11:35:43 +0100

    added QuestionnaireRepo

---
+++ backend/src/main/java/at/htl/repositories/QuestionnaireRepository.java
@@ -0,0 +1,7 @@
+package at.htl.repositories;
+
+import at.htl.model.Questionnaire;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
+public class QuestionnaireRepository implements PanacheRepository<Questionnaire> {
+}
