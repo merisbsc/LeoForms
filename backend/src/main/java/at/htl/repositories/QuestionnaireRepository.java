ae2566fe23707ac1e4088876b8414c5e51bee702
Meris Besic <besicmeris@gmail.com>
2022-01-03 13:17:27 +0100

    added QuestionnaireService

--- backend/src/main/java/at/htl/repositories/QuestionnaireRepository.java
+++ backend/src/main/java/at/htl/repositories/QuestionnaireRepository.java
@@ -7,4 +7,7 @@ import javax.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public class QuestionnaireRepository implements PanacheRepository<Questionnaire> {
+    public Questionnaire merge(Questionnaire questionnaire) {
+        return getEntityManager().merge(questionnaire);
+    }
 }
