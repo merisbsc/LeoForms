9ef52be25d3a723bb4a868dbd792d24e8a69a09d
Meris Besic <besicmeris@gmail.com>
2022-08-10 18:35:02 +0200

    added dummy saveAnswer Endpoint

---
+++ backend/src/main/java/at/htl/boundary/AnswerService.java
@@ -0,0 +1,35 @@
+package at.htl.boundary;
+
+import at.htl.model.Answer;
+import at.htl.repositories.AnswerRepository;
+
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+@Path("/answer")
+public class AnswerService {
+
+    @Inject
+    AnswerRepository answerRepository;
+
+    @GET
+    public List<Answer> getAllAnswers() {
+        return answerRepository.listAll();
+    }
+
+    @Transactional
+    @POST
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response saveAnswer(String answer) {
+        Answer a = new Answer(answer);
+        answerRepository.persist(a);
+
+        return Response.ok().build();
+
+    }
+}
