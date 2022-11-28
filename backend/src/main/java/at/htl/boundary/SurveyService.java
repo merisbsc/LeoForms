afe6c2727eff862e20a4af9e6a36bcc8008f01b9
thoefler <tobiashoefl@gmail.com>
2022-11-28 19:55:33 +0100

    Fresh frontend, fixed @Transactional on survey POST

--- backend/src/main/java/at/htl/boundary/SurveyService.java
+++ backend/src/main/java/at/htl/boundary/SurveyService.java
@@ -4,6 +4,7 @@ import at.htl.model.Survey;
 import at.htl.repositories.SurveyRepository;
 
 import javax.inject.Inject;
+import javax.transaction.Transactional;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -23,6 +24,7 @@ public class SurveyService {
         return surveyRepository.listAll();
     }
 
+    @Transactional
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     public Response saveSurvey(Survey survey) {
